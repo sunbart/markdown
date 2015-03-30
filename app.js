65b120c56981641b2f39b1c41525c1fd1d62d79d
sunbart <jan.bartonicek@praguecollege.cz>
2015-03-30 17:55:09 +0200

    first attempt at switching from getJSON to get for the api

--- app.js
+++ app.js
@@ -4,28 +4,17 @@ $(document).ready(function() {
 
   $("#mdInput").submit(function(event) {
     event.preventDefault();
-    var input = encodeURIComponent($("#mdText").val());
-    input = input.replace(/-/g,"%2D");
-    input = input.replace(/_/g,"%5F"); 
-    input = input.replace(/\*/g,"%2A"); 
-    input = input.replace(/~/g,"%7E");   
-    var toSend = 'f=parsedown&input=' + input;
+    var toSend = {'f': 'parsedown', 'input': $("#mdText").val()};
     console.log(toSend);
-    $.getJSON('api.php', toSend, function(reply) {
-      $("#outputWindow").empty();
+    $.get('api.php', toSend).done(function(reply) {
+      $("#outputWindow").html("");
       $.each(reply, function(key, val) {
         if(key == 'result'){
           $("#outputWindow").html(val);  
         }
       });
     }).fail(function(reply) {
-      console.error("error");
-      $("#outputWindow").empty();
-      $.each(reply, function(key, val) {
-        if(key == 'result'){
-          $("#outputWindow").html(val);  
-        }
-      });
+      console.error(reply);
     });
   });
   
